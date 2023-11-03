9b970e39b9d100ac523d730953734a18762f32a3
eddineimad0 <61315998+eddineimad0@users.noreply.github.com>
2023-11-03 18:15:35 +0100

    add more exports.

--- src/main.zig
+++ src/main.zig
@@ -5,4 +5,5 @@ const types = @import("types.zig");
 pub const Parser = parser.Parser;
 pub const TomlTable = types.Table;
 pub const TomlArray = types.Array(types.Value);
+pub const TomlValueType = types.ValueType;
 pub const ParserError = err.ParserError;
